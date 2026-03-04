43cf2ddbed6ed723ed3cd778bdeaf815cfc8e640
Ahlirek@outlook.com <sharker92@hotmail.com>
2026-03-03 18:41:38 -0600

    👌 IMPROVE: Remove forwards ref to update to React 19 way

--- gninnipsReact/src/components/Button.tsx
+++ gninnipsReact/src/components/Button.tsx
@@ -1,6 +1,5 @@
-import React, { forwardRef } from "react";
-import styles from "./Button.module.css";
-import HotkeyLabel from "./HotkeyLabel";
+import styles from './Button.module.css';
+import HotkeyLabel from './HotkeyLabel';
 
 interface ButtonProps {
   imgSrc?: string;
@@ -9,54 +8,47 @@ interface ButtonProps {
   buttonText?: string;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
   disabled?: boolean;
-  "data-index"?: string;
-  "data-button-text"?: string;
+  'data-index'?: string;
+  'data-button-text'?: string;
   title?: string;
+  ref?: React.Ref<HTMLButtonElement>;
 }
 
-const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  (
-    {
-      imgSrc,
-      emoji,
-      altText = "Image Button",
-      buttonText = "",
-      onClick,
-      disabled = false,
-      "data-index": dataIndex,
-      "data-button-text": dataButtonText,
-      title,
-    },
-    ref,
-  ) => {
-    return (
-      <button
-        ref={ref}
-        className={styles.imageButton}
-        onClick={onClick}
-        disabled={disabled}
-        aria-label={altText}
-        data-index={dataIndex}
-        data-button-text={dataButtonText}
-        title={title}
-      >
-        {imgSrc ? (
-          <img src={imgSrc} alt={altText} className={styles.buttonImage} />
-        ) : emoji ? (
-          <span className={styles.buttonEmoji}>{emoji}</span>
-        ) : null}
+export default function Button({
+  imgSrc,
+  emoji,
+  altText = 'Image Button',
+  buttonText = '',
+  onClick,
+  disabled = false,
+  'data-index': dataIndex,
+  'data-button-text': dataButtonText,
+  title,
+  ref,
+}: ButtonProps) {
+  return (
+    <button
+      ref={ref}
+      className={styles.imageButton}
+      onClick={onClick}
+      disabled={disabled}
+      aria-label={altText}
+      data-index={dataIndex}
+      data-button-text={dataButtonText}
+      title={title}
+    >
+      {imgSrc ? (
+        <img src={imgSrc} alt={altText} className={styles.buttonImage} />
+      ) : emoji ? (
+        <span className={styles.buttonEmoji}>{emoji}</span>
+      ) : null}
 
-        {buttonText && (
-          <span className={styles.buttonText}>
-            <HotkeyLabel text={buttonText} />
-          </span>
-        )}
-      </button>
-    );
-  },
-);
-
-Button.displayName = "Button";
-
-export default Button;
+      {buttonText && (
+        <span className={styles.buttonText}>
+          <HotkeyLabel text={buttonText} />
+        </span>
+      )}
+    </button>
+  );
+}
 
